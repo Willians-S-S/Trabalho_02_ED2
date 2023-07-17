c262c48f22202373ca97a305460b0dd7535b1f3c
Willians-S-S <09willians@gmail.com>
2023-07-17 09:32:25 -0300

    Bug: Inserir palavra

--- Arv23/arv23.h
+++ Arv23/arv23.h
@@ -23,6 +23,29 @@ struct  Arv23 {
     Arv23 *esq, *centro, *dir;
 };
 
-Arv23 *inserePalavra(Arv23 **raiz, char *palavra, int linha, Arv23 *pai, char *sobe, Linhas **listaSobe);
+Arv23 *criaNo(Info *info, Arv23 *noEsq, Arv23 *noCentro);
+
+void lerArquivo(char *path, Arv23 **raiz, int inseriu[]);
+
+Info *criaInfo(char *palavra, Linhas *lista, int linha);
 
 
+void adicionaNo(Arv23 **raiz, Info *info, Arv23 *filho);
+
+void inserirLinha(Linhas **no, int linha);
+
+Arv23 *quebraNo(Arv23 **Raiz, Arv23 *filho, Info *info, Info **infoSobe);
+
+Arv23 *inserePalavra(Arv23 **raiz, char *palavra, int linha, Arv23 *pai, Info **infoSobe);
+
+void auxiliaInsere(Arv23 **raiz, char *palavra, int linha, Arv23 *pai, Info **infoSobe);
+
+// imprimir
+void imprimirLista(Linhas *no);
+
+void imprimirInfo(Info *info);
+
+void imprimirArv(Arv23 *raiz);
+
+//busca
+Info *buscaPalavra(Arv23 *raiz, char *palavra);
